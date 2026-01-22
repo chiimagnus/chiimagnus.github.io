9bddba6b0777a6ced3296faa3b2f66f297761248
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2026-01-23 00:04:08 +0800

    refactor: 删除 3D 骰子相关组件及样式，简化项目结构

--- src-3d/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { D20Dice } from './D20Dice';
-export { DiceTray } from './DiceTray';
-export { DiceScene } from './DiceScene';
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@
 module.exports = {
   content: [
     "./index.html",
-    "./dice.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./src-3d/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
