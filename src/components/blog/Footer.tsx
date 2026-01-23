7b18a8a5a5b1cc81b83214a78f66f4a3a617306b
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2026-01-23 15:04:52 +0800

    fix: 更新页脚版权年份和更新时间

--- src/components/blog/Footer.tsx
+++ src/components/blog/Footer.tsx
@@ -4,9 +4,9 @@ const Footer: React.FC = () => {
   return (
     <footer className="text-white text-opacity-80 text-xs text-center py-4 px-8 mt-auto">
       <div className="flex justify-between items-center">
-        <span>© 2024~2025 𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼. 保留所有权利。</span>
+        <span>© 2024~2026 𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼. 保留所有权利。</span>
         <div className="flex items-center space-x-4">
-          <span>Update: 2025-07-30 Wednesday</span>
+          <span>Update: 2026-01-23 Friday</span>
         </div>
       </div>
     </footer>
